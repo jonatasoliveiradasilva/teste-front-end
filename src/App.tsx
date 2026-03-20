44099a0abc039ccc42ab99a998ac6f00ba112919
Jônatas Oliveira da Silva <OliveiraJonatas@Outlook.com.br>
2026-03-20 11:35:51 -0300

    feat: adiciona o arquivo principal do projeto

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Header from "../src/components/Header/Header";
+import Banner from "../src/components/Banner/Banner";
+
+import "./global.scss";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Banner />
+    </>
+  );
+}
+
+export default App;
